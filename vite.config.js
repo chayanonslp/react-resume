1695c2fcb28b850c48a227a759058470d89522df
chayanonslp <chayanon.slp@gmail.com>
2025-11-05 05:49:05 +0700

    move profile.webp and update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // base: '/', // เปลี่ยนเป็น '/' หรือ '/ชื่อ-repo/' ตามต้องการ
   base: '/react-resume/', // ถ้า deploy GitHub Pages เปลี่ยนเป็น '/ชื่อ-repo/'
 });
