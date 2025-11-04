60cf6ecf46d3322d31bc7fc0aeaa8a325ad4abe4
chayanonslp <109623321+chayanonslp@users.noreply.github.com>
2025-11-05 05:29:47 +0700

    Change base path for GitHub Pages deployment
    Updated base path for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ถ้า deploy GitHub Pages เปลี่ยนเป็น '/ชื่อ-repo/'
+  base: '/react-resume/', // ถ้า deploy GitHub Pages เปลี่ยนเป็น '/ชื่อ-repo/'
 });
