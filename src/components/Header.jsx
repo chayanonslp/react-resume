4a1d881c6ffd08c45251575ffb069a2608dfb0a0
chayanonslp <chayanon.slp@gmail.com>
2025-11-05 05:58:09 +0700

    Update Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
 export default function Header({ urlImg, name, role }) {
     return (
       <header className="text-center border-b pb-4">
-        <img src={urlImg}
-         alt={`${name} profile`}  className="w-32 h-32 rounded-full mx-auto mb-4 object-cover" />
+        <img src={`${import.meta.env.BASE_URL}profile.webp`} className="w-32 h-32 rounded-full mx-auto mb-4 object-cover" alt="Profile" />
         <h1 className="text-3xl font-bold">{name}</h1>
         <p className="text-blue-600 font-medium">{role}</p>
       </header>
